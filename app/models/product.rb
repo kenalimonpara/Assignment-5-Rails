4ac13de77b659afc730bb85a3d1a7fa252c58016
Kenali Monpara <kenalimonpara12345@gmail.com>
2020-10-02 15:26:27 -0500

    Validation complete

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  validates :title, presence :true,
+                    length: {minimum: 5}
+   validates :price, presence :true
+   validates :stock_quantity, presence :true
 end
